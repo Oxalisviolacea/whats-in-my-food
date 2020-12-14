3cac3ee1e6fa4e8997789d2bfd005521d4c5b53d
oxalisviolacea <hannadavis09@gmail.com>
2020-12-14 10:47:02 -0700

    Update to show total results

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,7 +1,7 @@
 class SearchFacade
   def self.foods(ingredient)
     json = FoodService.search_for_(ingredient)
-    json[:foods][0..9].map do |food_data|
+    json[:foods].map do |food_data|
       Food.new(food_data)
     end
   end
