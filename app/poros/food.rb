7222c2cf87613e1410a2822a5247ac5b9d9fe321
oxalisviolacea <hannadavis09@gmail.com>
2020-12-14 10:29:18 -0700

    Add food poros

---
+++ app/poros/food.rb
@@ -0,0 +1,9 @@
+class Food
+  attr_reader :code, :description, :brand, :ingredients
+  def initialize(attributes)
+    @code = attributes[:gtinUpc]
+    @description = attributes[:description]
+    @brand = attributes[:brandOwner]
+    @ingredients = attributes[:ingredients]
+  end
+end
