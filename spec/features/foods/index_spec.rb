4a74f0896a98260128dee75cd22c5647e53a2e0a
oxalisviolacea <hannadavis09@gmail.com>
2020-12-14 10:30:14 -0700

    Change test to fit new results

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe 'Foods Index Page', type: :feature do
       fill_in :q, with: 'sweet potatoes'
       click_on 'Search'
       expect(current_path).to eq('/foods')
-      expect(page).to have_content('Total Results: 39244')
+      expect(page).to have_content('Showing 10 results')
       expect(page).to have_content('GTIN/UPC Code: 070560951975')
       expect(page).to have_content('Description: SWEET POTATOES')
       expect(page).to have_content('Brand Owner: The Pictsweet Company')
-      expect(page).to have_content('Total lipid (fat)')
-      expect(page).to have_content('Vitamin D (D2 + D3), International Units')
+      expect(page).to have_content('Ingredients: SWEET POTATOES.')
     end
   end
 end
