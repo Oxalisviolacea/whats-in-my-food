e33b3203a3747271fc651a358d8dca3e3de83a67
oxalisviolacea <hannadavis09@gmail.com>
2020-12-14 10:28:22 -0700

    Refactor and send to search facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,5 @@
 class SearchController < ApplicationController
   def index
-    search = params[:q]
-    conn = Faraday.new("https://api.nal.usda.gov") do |faraday|
-      faraday.params["api_key"] = ENV["USDA_API_KEY"]
-    end
-    response = conn.get("/fdc/v1/foods/search?query=#{search}")
-    json = JSON.parse(response.body, symbolize_names: true)
-    @foods = json
+    @foods = SearchFacade.foods(params[:q])
   end
 end 
